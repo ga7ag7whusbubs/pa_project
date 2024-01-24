e727663f0a1970ab532d660c1a208f4eaca8f02c
ga7ag7whusbubs <151458385+ga7ag7whusbubs@users.noreply.github.com>
2024-01-24 17:45:37 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,24 +1,17 @@
-from flask import Flask, request, jsonify
-from app import simulate_elevator_continuous_full_motion
+from flask import Flask, Blueprint, request, jsonify
+from simulation import simulate_elevator_continuous_full_motion
 
-app_api = Flask(__name__)
+app_api = Blueprint("app_api", __name__)
 
-@app_api.route('/api/generate_chart', methods=['POST'])
+@app_api.route("/api/generate_chart", methods=["POST"])
 def generate_chart():
-    try:
-        data = request.json
-        load = float(data['load'])
-        max_floor_height = float(data['max_floor_height'])
-        Kp = float(data['Kp'])
-        Ki = float(data['Ki'])
-        Kd = float(data['Kd'])
-        elevator_weight = float(data['elevator_weight'])
-        avg_person_weight = float(data['avg_person_weight'])
-        simulation_time = float(data['simulation_time'])
+    data = request.get_json()
+    result_data = simulate_elevator_continuous_full_motion(
+        data["load"], data["max_floor_height"], data["Kp"],
+        data["Ki"], data["Kd"], data["elevator_weight"],
+        data["avg_person_weight"], data["simulation_time"]
+    )
 
-        result_data = simulate_elevator_continuous_full_motion(load, max_floor_height, Kp, Ki, Kd, elevator_weight,
-                                                               avg_person_weight, simulation_time)
-
-        return jsonify(result_data), 200
+    return jsonify(result_data)
     except Exception as e:
         return jsonify({'error': str(e)}), 400
