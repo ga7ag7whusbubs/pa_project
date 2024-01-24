1813aba3af88b6c8e0c3801daeef319a4f502545
ga7ag7whusbubs <151458385+ga7ag7whusbubs@users.noreply.github.com>
2024-01-24 17:50:13 +0100

    Update api.py

--- api.py
+++ api.py
@@ -5,13 +5,16 @@ app_api = Blueprint("app_api", __name__)
 
 @app_api.route("/api/generate_chart", methods=["POST"])
 def generate_chart():
-    data = request.get_json()
-    result_data = simulate_elevator_continuous_full_motion(
-        data["load"], data["max_floor_height"], data["Kp"],
-        data["Ki"], data["Kd"], data["elevator_weight"],
-        data["avg_person_weight"], data["simulation_time"]
-    )
+     try:
+        data = request.get_json()
+        result_data = simulate_elevator_continuous_full_motion(
+            data["load"], data["max_floor_height"], data["Kp"],
+            data["Ki"], data["Kd"], data["elevator_weight"],
+            data["avg_person_weight"], data["simulation_time"]
+        )
 
-    return jsonify(result_data)
+        return jsonify(result_data)
+    except KeyError as e:
+        return jsonify({'error': f'Missing key: {str(e)}'}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 400
